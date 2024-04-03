4b3e6dbe664873a55ec6d67606045310c956b6b5
kipling <kiplingmarich@gmail.com>
2024-04-03 18:08:08 +0300

    Init

---
+++ components/empty-screen.tsx
@@ -0,0 +1,37 @@
+import { UseChatHelpers } from 'ai/react'
+
+import { Button } from '@/components/ui/button'
+import { ExternalLink } from '@/components/external-link'
+import { IconArrowRight } from '@/components/ui/icons'
+
+const exampleMessages = [
+  {
+    heading: 'Explain technical concepts',
+    message: `What is a "serverless function"?`
+  },
+  {
+    heading: 'Summarize an article',
+    message: 'Summarize the following article for a 2nd grader: \n'
+  },
+  {
+    heading: 'Draft an email',
+    message: `Draft an email to my boss about the following: \n`
+  }
+]
+
+export function EmptyScreen() {
+  return (
+    <div className="mx-auto max-w-2xl px-4">
+      <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
+        <h1 className="text-lg font-semibold">
+          Welcome this is Kip's Assistant
+        </h1>
+
+        <p className="leading-normal text-muted-foreground">
+         I can take questions for Kelvin Kiprotich and can offer more
+         details about who he is as a person. 
+        </p>
+      </div>
+    </div>
+  )
+}
